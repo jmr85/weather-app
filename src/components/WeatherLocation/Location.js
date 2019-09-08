292294506eca504c247fba62c98de84ea04c7ae5
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2019-09-08 20:08:12 -0300

    class 34 External Font (fonts google)

--- src/components/WeatherLocation/Location.js
+++ src/components/WeatherLocation/Location.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
 
 const Location = ({city}) => (
     //Destructuring
-    <div>
+    <div className="locationCont">
         <h1>
             {city}
         </h1>
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import './styles.css';
 
 const WeatherLocation = () => (
-    <div>
+    // weatherLocationCont es lo mismo que index.js
+    <div className="weatherLocationCont">
         <Location city={"Barcelona"}></Location>
         <WeatherData></WeatherData>
     </div>
