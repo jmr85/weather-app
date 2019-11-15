5b981253ac00d74ffcd4228a46b6adf4c5c3e775
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2019-11-15 01:32:34 -0300

    92.  ForecastExtended: Indicador carga inicial

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
 import './styles.css'
 
+/*
 const days = [
     'Lunes',
     'Martes',
@@ -16,18 +17,31 @@ const data = {
     weatherState: 'normal',
     wind: 'normal',
 }
+*/
 class ForecastExtended extends Component {
     
+    constructor(){
+        super();
+        this.state = { forecastData: null }
+    }
+
     renderForecastItemDays(){
-        return days.map(day => (<ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>))
+        return "Render Items";
+        //return days.map(day => (<ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>))
+    }
+    renderProgress = () => {
+        return <h3>Cargando Pronostico extendido...</h3>;
     }
 
     render() {
         const {city} = this.props;
+        const {forecastData} = this.state;
         return (
         <div> 
             <h2 className="forecast-title">Pronostico extendido para {city}</h2> 
-            {this.renderForecastItemDays()}
+            {forecastData ? 
+                this.renderForecastItemDays(): 
+                this.renderProgress()}
         </div>);
     }
 }
